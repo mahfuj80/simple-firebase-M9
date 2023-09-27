8cfd147888eda307db680a0f673bbe59699690a8
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-28 02:13:09 +0600

    Open google login popup and Recap google popup sign

--- src/__steps.js
+++ src/__steps.js
@@ -6,8 +6,9 @@
  * 5. add config file to your project
  * 6. Danger: Do not publish or make firebase config public by pushing on github
  * 7. Visit: Go to docs > Build > Authentication > Web > Get Started
- * 8. export app from the firebase.confige.js file: export default app;
+ * 8. export app from the firebase.config.js file: export default app;
  * 9. Login.jsx: import getAuth from firebase/auth;
  * 10. create const auth = getAuth(app)
  * 11. import googleAuthProvider and create a new provider
+ * 12. use signInWithPopUp and pass auth and provider
  */
